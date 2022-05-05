ee8e445d2fd8fa20eecb4ec9d32cf753b2716ade
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2022-05-05 13:42:57 -0400

    display backend heroku data w/axios call set up hooks to show data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 // import FileEdit from "./Pages/Files/FileEdit";
 // import FileNew from "./Pages/Files/FileNew";
 // import FileShow from "./Pages/Files/FileShow";
-// import FilesIndex from "./Pages/Files/FilesIndex";
+import FilesIndex from "./Pages/Files/FilesIndex";
 import { apiURL } from "./util/apiURL.js";
 
 import "./App.css";
@@ -12,16 +13,34 @@ const API = apiURL();
 
 function App() {
 
+  const [files, setFiles] = useState([]);
+
   
-  const getFiles = async () => {
-    try {
-      const res = await axios.get(`${API}/files`);
-console.log(res.data,"res data files")
-    } catch (err) {
-      console.log(err);
-    }
-  };
-getFiles()
+ 
+    
+    const getFiles = async () => {
+      try {
+        const res = await axios.get(`${API}/files`);
+        setFiles(res.data.payload);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+
+    useEffect(() => {
+      getFiles();
+    }, []);
+
+
+
+
+  
+
+
+
+
+
 
 
   return (
@@ -29,6 +48,16 @@ getFiles()
     
         <BrowserRouter>
         Parent Files
+
+        <div className="fileList">
+      {files.map((file, index) => {
+        return ( 
+            <FilesIndex file={file} key={index}/>
+        )
+      })}
+    </div>
+
+
           {/* <>
             <BrowserRouter>
          
---
+++ src/Pages/Files/FilesIndex.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+const FilesIndex = ({ file }) => {
+
+  //alert the name of the students on clicking the card
+  const alertName = (event) => {
+    //event.preventDefault();
+    event.stopPropagatiaon();
+    console.log(event.target.innerText);
+    // alert(event.target.innerText);
+  }
+
+ 
+  return (
+    <div>
+          <div className="studentCard__name">
+          File Name: {file.child_name} 
+        </div>
+    </div>
+  );
+};
+
+export default FilesIndex;
